d056814a16a57f97bbd4f90cccffd20879e2d268
lxfly2000 <gaoboyuan8@qq.com>
2019-06-19 06:55:22 +0800

    Fix bat.

--- build.bat
+++ build.bat
@@ -1,16 +1,17 @@
 @echo off
-setlocal EnableDelayedExpansion
 copy %1\7zsfx.exe 7z\
 cd 7z
 7za a archive.7z .\*
 echo ;!@Install@!UTF-8!>>config.txt
 echo RunProgram="7zsfx.exe">>config.txt
 echo ;!@InstallEnd@!>>config.txt
-md ..\build
+cd ..
+md build
+setlocal EnableDelayedExpansion
 set name=7zsfx.exe
 if %1==Debug set name=7zsfxd.exe
-copy/b 7zS.sfx+config.txt+archive.7z ..\build\!name!
-cd ..\build
+copy/b 7z\7zS.sfx+7z\config.txt+7z\archive.7z build\!name!
+cd build
 for /f %%i in ('dir /b !name!') do echo 文件已输出至 %%~dpnxi
 cd ..
 del 7z\7zsfx.exe 7z\config.txt 7z\archive.7z
